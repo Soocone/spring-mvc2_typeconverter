97b428543dfb1c3dfc08c9dc0159089125736f1f
Soocone <wltn3185@naver.com>
2024-04-16 21:18:44 +0900

    4/16 commit

--- src/main/java/hello/typeconverter/converter/StringToIntegerConverter.java
+++ src/main/java/hello/typeconverter/converter/StringToIntegerConverter.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.converter.Converter;
 
 @Slf4j
-public class StringToIntegerConverter implements Converter<String, Integer> {
+public class StringToIntegerConverter implements Converter<String, Integer> { // String을 Integer로
 
     @Override
     public Integer convert(String source) {
